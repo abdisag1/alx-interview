4515276417ca17fee8a340e27e9d95753fdff13c
abdisag1 <myabdisa@gmail.com>
2022-06-17 04:58:21 +0300

    done pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,18 +1,26 @@
 #!/usr/bin/python3
-"""
-pascal_triangle
-"""
+""" Creates a function that returns a lits of lists
+of integers representing the Pascal's triangle of n """
 
 
 def pascal_triangle(n):
-    """
-    Return the Pascal's triangle of size n
-    """
-    triangle = []
+    """ creates a pascal triangle
+    n:
+        number of rows
+    return:
+        Pascal's triangle """
+    new_pascal = []
+
+    """ Assumes that n is an integer """
+    if n <= 0:
+        return new_pascal
+
     for i in range(n):
-        row = [1]
-        for j in range(i):
-            row.append(triangle[i-1][j] + triangle[i-1][j+1])
-        row.append(1)
-        triangle.append(row)
-    return triangle
+        row_index = [1]
+        if new_pascal:
+            final_row = new_pascal[-1]
+            row_index.extend([sum(pair) for pair in
+                              zip(final_row, final_row[1:])])
+            row_index.append(1)
+        new_pascal.append(row_index)
+    return (new_pascal)
