a64eec3e4e506b146cd6ae5574409f785b1bcd2d
abdisag1 <myabdisa@gmail.com>
2022-07-06 23:11:39 +0300

    len >6

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -8,7 +8,7 @@ possible_stat_code = {"200": 0, "301": 0, "400": 0,
 try:
     for line in sys.stdin:
         elements = line.split(" ")
-        if (len(elements) == 9):
+        if (len(elements) > 6):
 
             stat_code = elements[-2]
             if (stat_code in possible_stat_code.keys()):
